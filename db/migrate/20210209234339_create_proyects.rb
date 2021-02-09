92d6eccb19f4bf6ba5ab48d07116a595860fe4ba
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-02-09 20:45:32 -0300

    db add

---
+++ db/migrate/20210209234339_create_proyects.rb
@@ -0,0 +1,13 @@
+class CreateProyects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :proyects do |t|
+      t.string :name
+      t.string :description
+      t.integer :state, defalut: 0
+      t.date :initial_date
+      t.date :finish_date
+
+      t.timestamps
+    end
+  end
+end
