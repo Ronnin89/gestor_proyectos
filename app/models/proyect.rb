af0caab285b614db4e638c4f4480b7088e19c171
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-03-10 20:29:07 -0300

    minors fixed

--- app/models/proyect.rb
+++ app/models/proyect.rb
@@ -1,3 +1,6 @@
 class Proyect < ApplicationRecord
     enum state:[:propuesta, :en_progreso, :terminado]
+    validated :name, present: true
+    validated :description, present: true
+    validated :state, present: true
 end
